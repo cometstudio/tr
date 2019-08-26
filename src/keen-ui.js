df4580a297b6d40a49ad1b877b3226bc618191ec
Mister X <mister@x.com>
2019-08-26 18:58:24 +0300

    xx

--- src/keen-ui.js
+++ src/keen-ui.js
@@ -3,8 +3,9 @@ import KeenUI from 'keen-ui';
 import 'keen-ui/dist/keen-ui.css'; // All Keen UI components
 import 'keen-ui/src/bootstrap'; // Required to setup Keen UI, should be imported only once in your project
 
-import UiToolbar from 'keen-ui/src/UiToolbar.vue';
-import UiButton from 'keen-ui/src/UiButton.vue';
+import UiToolbar from 'keen-ui/src/UiToolbar';
+import UiButton from 'keen-ui/src/UiButton';
+import UiTextbox from 'keen-ui/src/UiTextbox';
 
 Vue.use(KeenUI, {
     UiToolbar: {
